8017e80acc8b1957bf55ddeadf1f01637bcb178e
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-12-12 22:32:39 -0600

    ci: Test on PyPy

--- tests/test_generic.py
+++ tests/test_generic.py
@@ -1,12 +1,13 @@
 from importlib.metadata import PathDistribution
 
 import pytest
-from hypothesis import given
+from hypothesis import HealthCheck, given, settings
 from hypothesis_jsonschema import from_schema
 
 from pep610 import read_from_distribution, write_to_distribution
 
 
+@settings(suppress_health_check=[HealthCheck.too_slow])
 @given(
     from_schema(
         {
