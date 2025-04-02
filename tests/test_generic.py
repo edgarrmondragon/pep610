208501f5ad08dc0b24cce75cac929d5de46608c0
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-04-02 00:58:10 -0600

    chore: Use uv and tox (#139)
    <!-- readthedocs-preview pep610 start -->
    ----
    📚 Documentation preview 📚:
    https://pep610--139.org.readthedocs.build/en/139/
    
    <!-- readthedocs-preview pep610 end -->

--- tests/test_generic.py
+++ tests/test_generic.py
@@ -1,12 +1,17 @@
+from __future__ import annotations
+
 import json
 import sys
+import typing as t
 from importlib.metadata import Distribution
 
-import pytest
 from hypothesis import HealthCheck, given, settings
 from hypothesis_jsonschema import from_schema
 
 from pep610 import read_from_distribution, write_to_distribution
+
+if t.TYPE_CHECKING:
+    import pytest
 
 if sys.version_info < (3, 9):
     import importlib_resources
@@ -19,7 +24,7 @@ SCHEMA = json.loads(SCHEMA_FILE.read_text())
 
 @settings(suppress_health_check=[HealthCheck.too_slow])
 @given(from_schema(SCHEMA))
-def test_generic(tmp_path_factory: pytest.TempPathFactory, value: dict):
+def test_generic(tmp_path_factory: pytest.TempPathFactory, value: dict[str, t.Any]) -> None:
     """Test parsing a local directory."""
     dist_path = tmp_path_factory.mktemp("pep610")
     dist = Distribution.at(dist_path)
